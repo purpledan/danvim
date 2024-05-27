745b5ea98255cd0439492e2dfd6a3a310aed1478
Daniel <221143711@student.uj.ac.za>
2024-05-27 15:50:50 +0200

    Added toggle map for inlay hints and changed map for dim mode

--- lua/dan/remap.lua
+++ lua/dan/remap.lua
@@ -2,6 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Switch for Twilight dm -> "dim mode"
-vim.keymap.set("n", "<leader>dm", vim.cmd.Twilight)
+vim.keymap.set("n", "<leader>td", vim.cmd.Twilight)
 
 
